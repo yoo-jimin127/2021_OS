fe0392d79f536d837f00f1086256e70a3a95688c
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-12 20:27:00 +0900

    commit sys_calc_mod.c

---
+++ assignment3/system_call_function/sys_calc_mod.c
@@ -0,0 +1,18 @@
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <asm/uaccess.h>
+
+asmlinkage long sys_calc_mod (long oprd1, long oprd2, long* mem_block_buf) {
+	long res;
+	res = oprd1 % oprd2;
+
+	if (copy_to_user(mem_block_buf, &res, sizeof(res))) {
+		return -EFAULT;
+	}
+
+	return 0;
+}
+
+SYSCALL_DEFINE3(calc_mod, long, oprd1, long, oprd2, long*, mem_block_buf) {
+	return sys_calc_mod(oprd1, oprd2, mem_block_buf);
+}
