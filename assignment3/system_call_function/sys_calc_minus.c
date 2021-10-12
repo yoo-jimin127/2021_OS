7814fe4ff0471711d40c92875d16dc45c89707e0
yoo-jimin127 <dbwlals9936@naver.com>
2021-10-12 20:26:26 +0900

    commit sys_calc_add.c

---
+++ assignment3/system_call_function/sys_calc_minus.c
@@ -0,0 +1,19 @@
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <asm/uaccess.h>
+
+asmlinkage long sys_calc_minus (long oprd1, long oprd2, long* mem_block_buf) {
+	long res;
+	res = oprd1 - oprd2;
+
+	if (copy_to_user(mem_block_buf, &res, sizeof(res))) {
+		return -EFAULT;
+	}
+
+	return 0;
+}
+
+SYSCALL_DEFINE3 (calc_minus, long, oprd1, long, oprd2, long*, mem_block_buf) {
+	return sys_calc_minus(oprd1, oprd2, mem_block_buf);
+}
+
