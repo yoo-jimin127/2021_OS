5ce9cc26cbb6b107b177c8e1146cf2a5204d96ef
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2021-11-21 00:21:05 +0900

    Add files via upload

--- assignment5/page_replacement.c
+++ assignment5/page_replacement.c
@@ -18,6 +18,8 @@ int main (void) {
 	char frame_cnt_str[5];
 	int frame_cnt = 0;
 	char refer_strbuf[61]; //except page frame, page reference string storage
+	int refer_buf[31] = { 0, };
+	int element_cnt = 0; //change string array -> int array count variable
 	
 	/* ------ about main function ------ */
 
@@ -28,7 +30,8 @@ int main (void) {
 		exit(1);
 	}
 
-	fgets(inputbuf, sizeof(inputbuf), fp); // read input.txt
+	fread(inputbuf, sizeof(inputbuf), 1, fp); // read input.txt
+	printf("inputbuf: %s\n", inputbuf);
 	fclose(fp); //file close
 
 	/* ------ used method &  page reference string print ------ */
@@ -38,32 +41,33 @@ int main (void) {
 	char *ptr = strtok(inputbuf, "\n"); //tokenize frame_cnt & page reference string
 	strcpy(frame_cnt_str, ptr); // string copy frame number info token
 	frame_cnt = atoi(frame_cnt_str); //frame_cnt string -> int
-	printf("frame_cnt: %d\n", frame_cnt);
 	
-	while (ptr != NULL) {
-		ptr = strtok(NULL, "\n");
-		printf("ptr: %s\n", ptr);
-		strcpy(refer_strbuf, ptr);
-		printf("refer_strbuf : %s\n", refer_strbuf);
+	ptr = strtok(NULL, "\n");
+	strcpy(refer_strbuf, ptr); //page reference string except frame cnt
+
+	char *intptr = strtok(refer_strbuf, " "); //공백자를 기준으로 토큰화
+
+	while(intptr != NULL) {
+		
 	}
 
 	printf("page reference string : %s\n", refer_strbuf);
 	
 	/* ------ apply algorithm method by option ------ */
 	if (strcmp(option, "OPT") == 0) {
-		OPT_algorithm(frame_cnt, &refer_strbuf);
+		OPT_algorithm(frame_cnt, refer_strbuf);
 	}
 
 	else if (strcmp(option, "FIFO") == 0) {
-		FIFO_algorithm(frame_cnt, &refer_strbuf);
+		FIFO_algorithm(frame_cnt, refer_strbuf);
 	}
 
 	else if (strcmp(option, "LRU") == 0) {
-		LRU_algorithm(frame_cnt, &refer_strbuf);
+		LRU_algorithm(frame_cnt, refer_strbuf);
 	}
 
 	else if (strcmp(option, "Second-Chance") == 0) {
-		SecondChance_algorithm(frame_cnt, &refer_strbuf);
+		SecondChance_algorithm(frame_cnt, refer_strbuf);
 	}
 
 	else {
