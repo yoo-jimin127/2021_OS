3c387e8434ef712f2907adb56b252ff11d4a95ce
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2021-11-21 12:01:27 +0900

    LRU 구현 완료

--- assignment5/page_replacement.c
+++ assignment5/page_replacement.c
@@ -154,7 +154,7 @@ void FIFO_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 	}
 	memset(fault_check, ' ', sizeof(fault_check)); //fault_check 배열 초기화
 
-	for (int i = 0; i < element_cnt; i++) { //pagr reference string만큼 반복
+	for (int i = 0; i < element_cnt; i++) { //page reference string만큼 반복
 		memset(level_print, 0, sizeof(level_print));
 
 		if(CheckExist(frame_buf, frame_cnt, refer_buf[i]) == -1) { //비어있는 frame을 발견한 경우
@@ -165,6 +165,7 @@ void FIFO_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 		}
 		
 		sprintf(level_print,"%d\t\t", i+1);
+		
 		if (frame_buf[0] == -1) sprintf(tmp_print, " \t");
 		else sprintf(tmp_print, "%d\t", frame_buf[0]);
 		strcat(level_print, tmp_print);
@@ -191,6 +192,73 @@ void FIFO_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 
 /* ------ LRU altorithm ------*/
 void LRU_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
+	int *frame_buf = malloc(sizeof(int) * frame_cnt); //frame buf 동적할당
+	char *fault_check = malloc(sizeof(char) * element_cnt); //page fault의 발생 여부를 저장하는 배열
+	char *level_print = malloc(sizeof(char) * 300); //각 단계에서 page replacement 의 상태를 출력하는 메시지
+	char *tmp_print = malloc(sizeof(char) * 10); //문자열 저장을 위한 임시 버퍼
+
+	int pagefault_cnt = 0; //page fault counter
+	int currIdx = 0; //current index number
+	int totalCurr = 0; //가장 오래 참조되지 않은 element를 사용하기 위해 idx 값 조절 용도로 사용되는 변수
+	
+	for (int i = 0; i < frame_cnt; i++) {
+		frame_buf[i] = -1; //frame_buf를 -1으로 초기화
+	}
+
+	memset(fault_check, ' ', sizeof(fault_check)); //fault_check 배열 초기화
+
+	for (int i = 0; i < element_cnt; i++) { //page reference string만큼 반복
+		memset(level_print, 0, sizeof(level_print)); //단계별로 출력되는 메시지버퍼 초기화
+
+		if (CheckExist(frame_buf, frame_cnt, refer_buf[i]) != -1) { //refer_buf[i]의 메모리가 page frame상에 존재하는 경우
+			int tmp_mem = frame_buf[currIdx]; //참조된 page의 값을 임시 저장
+			for (int j = currIdx; j < totalCurr - 1; j++) {
+				frame_buf[j] = frame_buf[j+1]; //참조된 페이지는 그 다음 참조될 페이지로 선택되지 않도록 뒤로 옮기는 작업
+			}
+			frame_buf[totalCurr - 1] = tmp_mem; 
+		}
+
+		else { //page fault가 발생하며 refer_buf[i]의 메모리가 페이지 상에 올라가는 경우
+			if (frame_cnt == totalCurr) { //page replacement가 발생하는 경우(빈 frame 존재 X)
+				for (int j = 0; j < frame_cnt; j++) {
+					frame_buf[j - 1] = frame_buf[j]; //각 page frame에 저장된 내용을 앞으로 한칸씩 이동시키는 작업 우선 수행
+				}
+				frame_buf[frame_cnt - 1] = refer_buf[i]; //가장 뒤에 위치한 frame에 refer_buf[i] 인덱스의 메모리 삽입
+				fault_check[i] = 'F'; //해당 i번째 element에서의 page fault 발생여부 체크
+			}
+
+			else { //빈 frame이 존재하는 경우
+				frame_buf[totalCurr] = refer_buf[i]; //갱신된 totalCurr번째 인덱스에 메모리 삽입
+				fault_check[i] = 'F'; //해당 i번째 element에서의 page fault 발생여부 체크
+				totalCurr++; // totalCurr의 값 업데이트
+			}
+
+			pagefault_cnt++; //pagefault_cnt 증가
+		}
+
+		sprintf(level_print,"%d\t\t", i+1);
+		
+		if (frame_buf[0] == -1) sprintf(tmp_print, " \t");
+		else sprintf(tmp_print, "%d\t", frame_buf[0]);
+		strcat(level_print, tmp_print);
+		memset(tmp_print, 0, sizeof(tmp_print));
+		
+		if (frame_buf[1] == -1) sprintf(tmp_print, " \t");
+		else sprintf(tmp_print, "%d\t", frame_buf[1]);
+		strcat(level_print, tmp_print);
+		memset(tmp_print, 0, sizeof(tmp_print));
+		
+		if (frame_buf[2] == -1) sprintf(tmp_print, " \t");
+		else sprintf(tmp_print, "%d\t", frame_buf[2]);
+		strcat(level_print, tmp_print);
+		memset(tmp_print, 0, sizeof(tmp_print));
+		sprintf(tmp_print, "%c", fault_check[i]);
+		
+		strcat(level_print, tmp_print);
+		printf("%s\n", level_print);
+	}
+
+	printf("Number of page faults : %d times\n", pagefault_cnt);
 
 }
 
