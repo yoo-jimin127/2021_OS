cfa0840cfafe380f088328215fb1cd0f8a75be0c
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2021-11-21 13:27:08 +0900

    OPT level 출력부 수정

--- assignment5/page_replacement.c
+++ assignment5/page_replacement.c
@@ -10,7 +10,6 @@ void FIFO_algorithm(int frame_cnt, int element_cnt, int *refer_buf);
 void LRU_algorithm(int frame_cnt, int element_cnt, int *refer_buf);
 void SecondChance_algorithm(int frame_cnt, int element_cnt, int *refer_buf);
 int CheckExist(int *frame_buf, int frame_cnt, int refer_string);
-int CalcFrameLongest(int idx, int element_cnt, int frame_element, int *refer_buf);
 
 int main (void) {
 	/* ------ 파일 관련 변수 ------ */
@@ -85,7 +84,7 @@ int main (void) {
 
 /* ------ optimal altorithm ------*/
 void OPT_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
-	int *frame_buf = malloc(sizeof(int) * frame_cnt); // frame_buf 동적할당
+int *frame_buf = malloc(sizeof(int) * frame_cnt); // frame_buf 동적할당
 	char *fault_check = malloc(sizeof(char) * element_cnt); //pagefault의 발생여부를 저장하는 배열
 	char *level_print = malloc(sizeof(char) * 300); //각 단계에서의 page replacement 상태를 출력하는 메시지
 	char *tmp_print = malloc(sizeof(char) * 10); //문자열 저장을 위한 임시 버퍼
@@ -102,7 +101,7 @@ void OPT_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 	
 	memset(fault_check, ' ', sizeof(fault_check)); //fault_check 배열 초기화
 
-	for (int i = 0; i < frame_cnt; i++) {
+	for (int i = 0; i < element_cnt; i++) {
 		memset(level_print, 0, sizeof(level_print));
 		longest_frameNum = 0; //가장 오랫동안 참조되지 않은 frame의 index number 초기화
 		
@@ -115,12 +114,52 @@ void OPT_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 			}
 
 			else { //page fault가 발생하지 않는 경우
+				sprintf(level_print,"%d\t\t", i+1);
+				if (frame_buf[0] == -1) sprintf(tmp_print, " \t");
+		         else sprintf(tmp_print, "%d\t", frame_buf[0]);
+		         strcat(level_print, tmp_print);
+		         memset(tmp_print, 0, sizeof(tmp_print));
+ 
+				if (frame_buf[1] == -1) sprintf(tmp_print, " \t");
+		         else sprintf(tmp_print, "%d\t", frame_buf[1]);
+		         strcat(level_print, tmp_print);
+		         memset(tmp_print, 0, sizeof(tmp_print));
+ 
+		         if (frame_buf[2] == -1) sprintf(tmp_print, " \t");
+		         else sprintf(tmp_print, "%d\t", frame_buf[2]);
+		         strcat(level_print, tmp_print);
+		         memset(tmp_print, 0, sizeof(tmp_print));
+		         sprintf(tmp_print, "%c", fault_check[i]);
+ 
+		         strcat(level_print, tmp_print);
+		         printf("%s\n", level_print);
+
 				continue;
 			}
 		}
 
 		else { //OPTIMAL Page replacement 발생
 			if(CheckExist(frame_buf, frame_cnt, refer_buf[i]) != -1) { //빈 frame이 존재하지 않는 경우
+				sprintf(level_print,"%d\t\t", i+1);
+                if (frame_buf[0] == -1) sprintf(tmp_print, " \t");
+                else sprintf(tmp_print, "%d\t", frame_buf[0]);
+				strcat(level_print, tmp_print);
+                memset(tmp_print, 0, sizeof(tmp_print));
+ 
+                if (frame_buf[1] == -1) sprintf(tmp_print, " \t");
+                else sprintf(tmp_print, "%d\t", frame_buf[1]);
+                strcat(level_print, tmp_print);
+                memset(tmp_print, 0, sizeof(tmp_print));
+ 
+                if (frame_buf[2] == -1) sprintf(tmp_print, " \t");
+                else sprintf(tmp_print, "%d\t", frame_buf[2]);
+                strcat(level_print, tmp_print);
+                memset(tmp_print, 0, sizeof(tmp_print));
+                sprintf(tmp_print, "%c", fault_check[i]);
+ 
+                strcat(level_print, tmp_print);
+                printf("%s\n", level_print);
+
 				continue;
 			}
 
@@ -160,55 +199,29 @@ void OPT_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 	}
 
 	printf("Number of page faults : %d times\n", pagefault_cnt);
-
 }
 
 /* ------ FIFO altorithm ------*/
 void FIFO_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 	int *frame_buf = malloc(sizeof(int) * frame_cnt); //frame buf 동적할당
 	char *fault_check = malloc(sizeof(char) * element_cnt); //page fault의 발생여부를 저장하는 배열
-	char *level_print = malloc(sizeof(char) * 300); //각 단계에서의 page replacement 상태를 출력하는 메시지
-	char *tmp_print = malloc(sizeof(char) * 10); //문자열 저장을 위한 임시 버퍼
-
 	int pagefault_cnt = 0; //page fault counter
 	int currIdx = 0; //page reference string을 삽입하는 인덱스
 
 	for (int i = 0; i < frame_cnt; i++) {
-		frame_buf[i] = -1; //frame_buf를 -1으로 초기화
+		frame_buf[i] = 0; //frame_buf를 0으로 초기화
+		frame_check[i] = 0; //비어있음을 의미하도록 frame_check의 모든 원소를 0으로 초기화
 	}
 	memset(fault_check, ' ', sizeof(fault_check)); //fault_check 배열 초기화
 
-	for (int i = 0; i < element_cnt; i++) { //page reference string만큼 반복
-		memset(level_print, 0, sizeof(level_print));
-
+	for (int i = 0; i < element_cnt; i++) { //pagr reference string만큼 반복
 		if(CheckExist(frame_buf, frame_cnt, refer_buf[i]) == -1) { //비어있는 frame을 발견한 경우
 			frame_buf[currIdx] = refer_buf[i]; //currIdx의 위치에 해당 reference string을 넣음
 			currIdx = (currIdx + 1) % frame_cnt; //currIdx의 값 증가
-			pagefault_cnt++; //pagefault의 카운터 증가
-			fault_check[i] = 'F'; //해당 i번째 element에서의 page fault 발생여부 체크
+			pagefault_cnt++;
+			fault_check[i] = 'F';
 		}
-		
-		sprintf(level_print,"%d\t\t", i+1);
-		
-		if (frame_buf[0] == -1) sprintf(tmp_print, " \t");
-		else sprintf(tmp_print, "%d\t", frame_buf[0]);
-		strcat(level_print, tmp_print);
-		memset(tmp_print, 0, sizeof(tmp_print));
-		
-		if (frame_buf[1] == -1) sprintf(tmp_print, " \t");
-		else sprintf(tmp_print, "%d\t", frame_buf[1]);
-		strcat(level_print, tmp_print);
-		memset(tmp_print, 0, sizeof(tmp_print));
-		
-		if (frame_buf[2] == -1) sprintf(tmp_print, " \t");
-		else sprintf(tmp_print, "%d\t", frame_buf[2]);
-		strcat(level_print, tmp_print);
-		memset(tmp_print, 0, sizeof(tmp_print));
-		
-		sprintf(tmp_print, "%c", fault_check[i]);
-		strcat(level_print, tmp_print);
-		
-		printf("%s\n", level_print);
+		printf("%d\t\t%d\t%d\t%d\t%c\n", i+1, frame_buf[0], frame_buf[1], frame_buf[2], fault_check[i]);
 	}
 
 	printf("Number of page faults : %d times\n", pagefault_cnt);
@@ -216,94 +229,14 @@ void FIFO_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
 
 /* ------ LRU altorithm ------*/
 void LRU_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
-	int *frame_buf = malloc(sizeof(int) * frame_cnt); //frame buf 동적할당
-	char *fault_check = malloc(sizeof(char) * element_cnt); //page fault의 발생 여부를 저장하는 배열
-	char *level_print = malloc(sizeof(char) * 300); //각 단계에서 page replacement 의 상태를 출력하는 메시지
-	char *tmp_print = malloc(sizeof(char) * 10); //문자열 저장을 위한 임시 버퍼
-
-	int pagefault_cnt = 0; //page fault counter
-	int currIdx = 0; //current index number
-	int totalCurr = 0; //가장 오래 참조되지 않은 element를 사용하기 위해 idx 값 조절 용도로 사용되는 변수
-	
-	for (int i = 0; i < frame_cnt; i++) {
-		frame_buf[i] = -1; //frame_buf를 -1으로 초기화
-	}
-
-	memset(fault_check, ' ', sizeof(fault_check)); //fault_check 배열 초기화
-
-	for (int i = 0; i < element_cnt; i++) { //page reference string만큼 반복
-		memset(level_print, 0, sizeof(level_print)); //단계별로 출력되는 메시지버퍼 초기화
-
-		if (CheckExist(frame_buf, frame_cnt, refer_buf[i]) != -1) { //refer_buf[i]의 메모리가 page frame상에 존재하는 경우
-			int tmp_mem = frame_buf[currIdx]; //참조된 page의 값을 임시 저장
-			for (int j = currIdx; j < totalCurr - 1; j++) {
-				frame_buf[j] = frame_buf[j+1]; //참조된 페이지는 그 다음 참조될 페이지로 선택되지 않도록 뒤로 옮기는 작업
-			}
-			frame_buf[totalCurr - 1] = tmp_mem; 
-		}
-
-		else { //page fault가 발생하며 refer_buf[i]의 메모리가 페이지 상에 올라가는 경우
-			if (frame_cnt == totalCurr) { //page replacement가 발생하는 경우(빈 frame 존재 X)
-				for (int j = 0; j < frame_cnt; j++) {
-					frame_buf[j - 1] = frame_buf[j]; //각 page frame에 저장된 내용을 앞으로 한칸씩 이동시키는 작업 우선 수행
-				}
-				frame_buf[frame_cnt - 1] = refer_buf[i]; //가장 뒤에 위치한 frame에 refer_buf[i] 인덱스의 메모리 삽입
-				fault_check[i] = 'F'; //해당 i번째 element에서의 page fault 발생여부 체크
-			}
-
-			else { //빈 frame이 존재하는 경우
-				frame_buf[totalCurr] = refer_buf[i]; //갱신된 totalCurr번째 인덱스에 메모리 삽입
-				fault_check[i] = 'F'; //해당 i번째 element에서의 page fault 발생여부 체크
-				totalCurr++; // totalCurr의 값 업데이트
-			}
-
-			pagefault_cnt++; //pagefault_cnt 증가
-		}
-
-		sprintf(level_print,"%d\t\t", i+1);
-		
-		if (frame_buf[0] == -1) sprintf(tmp_print, " \t");
-		else sprintf(tmp_print, "%d\t", frame_buf[0]);
-		strcat(level_print, tmp_print);
-		memset(tmp_print, 0, sizeof(tmp_print));
-		
-		if (frame_buf[1] == -1) sprintf(tmp_print, " \t");
-		else sprintf(tmp_print, "%d\t", frame_buf[1]);
-		strcat(level_print, tmp_print);
-		memset(tmp_print, 0, sizeof(tmp_print));
-		
-		if (frame_buf[2] == -1) sprintf(tmp_print, " \t");
-		else sprintf(tmp_print, "%d\t", frame_buf[2]);
-		strcat(level_print, tmp_print);
-		memset(tmp_print, 0, sizeof(tmp_print));
-		sprintf(tmp_print, "%c", fault_check[i]);
-		
-		strcat(level_print, tmp_print);
-		printf("%s\n", level_print);
-	}
-
-	printf("Number of page faults : %d times\n", pagefault_cnt);
 
 }
 
-/* ------ Second Chance altorithm ------ */
+/* ------ Second Chance altorithm ------*/
 void SecondChance_algorithm(int frame_cnt, int element_cnt, int *refer_buf) {
-	int *frame_buf = malloc(sizeof(int) * frame_cnt); //frame buf 동적 할당
-	char *fault_check = malloc(sizeof(char) * element_cnt); //page fault의 발생 여부를 저장하는 배열
-	char *level_print = malloc(sizeof(char) * 300); //각 단계에서의 page replacement 상태를 출력하는 메시지
-	char *tmp_print = malloc(sizeof(char) * 10); //문자열 저장을 위한 임시 버퍼
 
-	int pagefault_cnt = 0; //page fault counter
-	int currIdx = 0; //page reference string을 삽입하는 인덱스
-
-	for (int i = 0; i < frame_cnt; i++) {
-		frame_buf[i] = -1; //frame_buf를 -1으로 초기화
-	}
-
-	memset(fault_check, ' ', sizeof(fault_check));
 }
 
-/* ------ frame의 empty 여부 확인 함수 ------ */
 int CheckExist (int *frame_buf, int frame_cnt, int refer_string) {
 	for (int i = 0; i < frame_cnt; i++) {
 		if (frame_buf[i] == refer_string) {
@@ -312,19 +245,4 @@ int CheckExist (int *frame_buf, int frame_cnt, int refer_string) {
 	}
 		
 		return -1;
-}
-
-/* ------ OPT 알고리즘: 앞으로 가장 오래 참조되지 않을 framenum 계산 함수 ------ */
-int CalcFrameLongest (int idx, int element_cnt, int frame_element, int *refer_buf) {
-	int frame_distance = 0; //각 frame의 element가 다음 참조까지 걸리는 시간 저장 변수
-	for (int i = idx + 1; i < element_cnt; i++) {
-		if (frame_element != refer_buf[i]) {
-			frame_distance++;
-		}
-
-		else {
-			break;
-		}
-	}
-		return frame_distance++;
 }
